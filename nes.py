e3a3471ded3a1a27c160b8d4ed93d004ac8c60f6
Kingfish Threesevens <scott.malec@uth.tmc.edu>
2014-12-16 16:39:38 -0600

    Update nes.py

--- nes.py
+++ nes.py
@@ -2,12 +2,13 @@ import nltk
 import sys
 from nltk.tokenize import RegexpTokenizer
 
-# sentence = "I am the walrus. I am the eggman. The man eats the apple. Ivan shot the bear."
+sentence = "I am the walrus. I am the eggman. The man eats the apple. Ivan shot the bear."
+
 for arg in sys.argv: 
     print arg
-sentences = nltk.sent_tokenize(arg)
-print(sentences)
-tokenizer = RegexpTokenizer(r'\w+')
+    sentences = nltk.sent_tokenize(arg)
+    print(sentences)
+    tokenizer = RegexpTokenizer(r'\w+')
 
 for sentence in sentences: 
  #   tokens = nltk.word_tokenize(sentence) 
